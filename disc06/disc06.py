93b629552d9f18662091e760bf7bd1352d119eb9
yyaooayy <1481905367@qq.com>
2023-05-13 15:17:15 +0800

    disc06
    disc06 已完成

---
+++ disc06/disc06.py
@@ -0,0 +1,128 @@
+def add_this_many(x, el, s):
+    """ Adds el to the end of s the number of times x occurs in s.
+    >>> s = [1, 2, 4, 2, 1]
+    >>> add_this_many(1, 5, s)
+    >>> s
+    [1, 2, 4, 2, 1, 5, 5]
+    >>> add_this_many(2, 2, s)
+    >>> s
+    [1, 2, 4, 2, 1, 5, 5, 2, 2]
+    """
+    num = 0
+    for i in s:
+        if i == x:
+            num += 1
+    j = 0
+    while j < num:
+        s.append(el)
+        j += 1
+
+    
+def filter_iter(iterable, f):
+    """
+    >>> is_even = lambda x: x % 2 == 0
+    >>> list(filter_iter(range(5), is_even)) # a list of the values yielded from the call to filter_iter
+    [0, 2, 4]
+    >>> all_odd = (2*y-1 for y in range(5))
+    >>> list(filter_iter(all_odd, is_even))
+    []
+    >>> naturals = (n for n in range(1, 100))
+    >>> s = filter_iter(naturals, is_even)
+    >>> next(s)
+    2
+    >>> next(s)
+    4
+    """
+    for i in iterable:
+        if f(i):
+            yield i
+
+
+def is_prime(n):
+    """Returns True if n is a prime number and False otherwise.
+    >>> is_prime(2)
+    True
+    >>> is_prime(16)
+    False
+    >>> is_prime(521)
+    True
+    """
+    def helper(i):
+        if i > (n ** 0.5): # Could replace with i == n
+            return True
+        elif n % i == 0:
+            return False
+        return helper(i + 1)
+    return helper(2)
+
+def primes_gen(n):
+    """Generates primes in decreasing order.
+    >>> pg = primes_gen(7)
+    >>> list(pg)
+    [7, 5, 3, 2]
+    """
+    # while n > 1:
+    #     if is_prime(n):
+    #         yield n
+    #     n -= 1
+    for num in range(n, 1, -1):
+        if is_prime(num):
+            yield num
+
+
+def primes_gen(n):
+    """Generates primes in decreasing order.
+    >>> pg = primes_gen(7)
+    >>> list(pg)
+    [7, 5, 3, 2]
+    """
+    if n == 1:
+        return
+    if is_prime(n):
+        yield n
+    yield from primes_gen(n - 1)
+
+
+def preorder(t):
+    """Return a list of the entries in this tree in the order that they
+    would be visited by a preorder traversal (see problem description).
+
+    >>> numbers = tree(1, [tree(2), tree(3, [tree(4), tree(5)]), tree(6, [tree(7)])])
+    >>> preorder(numbers)
+    [1, 2, 3, 4, 5, 6, 7]
+    >>> preorder(tree(2, [tree(4, [tree(6)])]))
+    [2, 4, 6]
+    """
+    # lst = []
+    # lst += [label(t)]
+    # lst += [preorder(i) for i in branches(t)]
+    # return lst
+    lst = []
+    for i in branches(t):
+        lst += preorder(i)
+    return [label(t)] + lst
+
+
+def generate_preorder(t):
+    """Yield the entries in this tree in the order that they
+    would be visited by a preorder traversal (see problem description).
+
+    >>> numbers = tree(1, [tree(2), tree(3, [tree(4), tree(5)]), tree(6, [tree(7)])])
+    >>> gen = generate_preorder(numbers)
+    >>> next(gen)
+    1
+    >>> list(gen)
+    [2, 3, 4, 5, 6, 7]
+    """
+    yield label(t)
+    for i in branches(t):
+        yield from generate_preorder(i)
+
+
+def mystery(p, q):
+    p[1].extend(q)
+    q.append(p[1:])
+
+p = [2, 3]
+q = [4, [p]]
+mystery(q, p)
